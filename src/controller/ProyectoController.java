b8ca710d36aa574d5f66247cca81920b2cbf88cc
Javier Machado LNX <jamachadolopez@iesmordefuentes.com>
2025-06-04 12:49:05 +0200

    cambios javier

--- src/controller/ProyectoController.java
+++ src/controller/ProyectoController.java
@@ -1,4 +1,18 @@
 package controller;
 
+import dao.ProyectoDAO;
+
+import java.util.Scanner;
+
 public class ProyectoController {
+
+    private final ProyectoDAO dao = new ProyectoDAO();
+
+    public void anadirProyecto(){
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+    }
 }
