ed28afcd617af7aecb63d7eafbf98f18f2162edb
Javier Machado LNX <jamachadolopez@iesmordefuentes.com>
2025-06-04 12:59:11 +0200

    cambios

--- src/controller/ProyectoController.java
+++ src/controller/ProyectoController.java
@@ -11,8 +11,21 @@ public class ProyectoController {
     public void anadirProyecto(){
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Ingrese el titulo del proyecto: ");
+        String titulo = sc.nextLine();
+
+        System.out.println("Ingrese descripcion del proyecto: ");
+        String descripcion = sc.nextLine();
+
+        System.out.println("Ingrese la fecha de presentacion: ");
+
+        System.out.println("Precio: ");
+        double precio = sc.nextDouble();
 
 
+        //Viaje myviaje = new Viaje(destino, agencia, duracion, precio);
+        //dao.guardar(myviaje);
+        //System.out.println("Viaje insertado en BBDD");
 
     }
 }
--- src/dao/ProyectoDAO.java
+++ src/dao/ProyectoDAO.java
@@ -2,19 +2,37 @@ package dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import model.Equipo;
 import model.Proyecto;
+
+import java.util.List;
 
 public class ProyectoDAO {
 
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("miUnidadDePersistencia");
+    private EntityManager entityManager;
 
-    public void guardarProyecto(Proyecto proyecto) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(proyecto);
-        em.getTransaction().commit();
-        em.close();
+    public ProyectoDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public List<Proyecto> getAllProyecto() {
+        return entityManager.createQuery("SELECT p FROM Proyecto p", Proyecto.class).getResultList();
+    }
+
+    public void createProyecto(Proyecto pr) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.persist(pr);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e; // Propagamos la excepción
+        }
     }
 
 }
