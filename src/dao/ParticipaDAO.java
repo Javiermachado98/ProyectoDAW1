1fa5618024e9e225bded2185800266ece37b3979
Javier Machado LNX <jamachadolopez@iesmordefuentes.com>
2025-06-04 13:01:06 +0200

    participaDAO

--- src/dao/ParticipaDAO.java
+++ src/dao/ParticipaDAO.java
@@ -2,19 +2,37 @@ package dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import model.Equipo;
 import model.Participa;
+
+import java.util.List;
 
 public class ParticipaDAO {
 
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("miUnidadDePersistencia");
+    private EntityManager entityManager;
 
-    public void guardarParticipa(Participa participa) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(participa);
-        em.getTransaction().commit();
-        em.close();
+    public ParticipaDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public List<Participa> getAllParticipa() {
+        return entityManager.createQuery("SELECT p FROM Participa p", Participa.class).getResultList();
+    }
+
+    public void createParticipa(Participa pa) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.persist(pa);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e; // Propagamos la excepción
+        }
     }
 
 }
