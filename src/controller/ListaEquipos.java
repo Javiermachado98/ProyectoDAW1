2c43be3aa31fbbaf2416ae63e50941344e387afa
“nesss_27_” <naisalopez@iesmordefuentes.com>
2025-06-04 13:32:43 +0200

    Nestor

---
+++ src/controller/ListaEquipos.java
@@ -0,0 +1,14 @@
+package controller;
+
+import dao.LibreriaDao;
+import jakarta.persistence.EntityManager;
+import model.Libreria;
+import util.JPAUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class ListaEquipos {
+
+
+}
